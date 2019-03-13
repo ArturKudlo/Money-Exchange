804bf90371bef768a3694cda8f8a21bd55be7a39
Artur Kudla <arturkapes@gmail.com>
2019-03-13 10:35:02 +0300

    feature: add exchange logic

--- src/index.js
+++ src/index.js
@@ -1,5 +1,44 @@
-// PLEASE DON'T change function name
 module.exports = function makeExchange(currency) {
-    // Your code goes here!
-    // Return an object containing the minimum number of coins needed to make change
+    var money = {
+        H: 50,
+        Q: 25,
+        D: 10,
+        N: 5,
+        P: 1
+    };
+    var change = {};
+
+    if (currency === 0) {
+        return change;
+    }
+
+    if (currency > 10000) {
+        return {error: "You are rich, my friend! We don't have so much coins for exchange"}
+    }
+   
+    if (currency >= money.H) {
+        change.H = Math.floor(currency / money.H);
+        currency = currency % money.H;
+    }
+
+    if (currency >= money.Q) {
+        change.Q = Math.floor(currency / money.Q);
+        currency = currency % money.Q;
+    }
+
+    if (currency >= money.D) {
+        change.D = Math.floor(currency / money.D);
+        currency = currency % money.D;
+    }
+
+    if (currency >= money.N) {
+        change.N = Math.floor(currency / money.N);
+        currency = currency % money.N;
+    }
+
+    if (currency >= money.P) {
+        change.P = Number(currency);
+    }
+
+    return change;
 }
